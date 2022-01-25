2a896b17ea15904c698202f48709a0eded1c84e2
Yi-Ning <ning19950212@gmail.com>
2022-01-25 16:37:50 +0800

    nodejs

--- practice/env2.js
+++ practice/env2.js
@@ -1,4 +1,7 @@
 require('dotenv').config()
+//dotenv 會尋找專案裡的.env
+//require('dotenv').config({ path: '/custom/path/to/.env' })
+//有可能有多個.env檔，path指定哪個.env的路徑
 
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASS);
