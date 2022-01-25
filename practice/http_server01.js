b16ad1f3e5db426c4f9ed751aaf2a694f7666b33
Yi-Ning <ning19950212@gmail.com>
2022-01-25 14:25:41 +0800

    nodejs

--- practice/http_server01.js
+++ practice/http_server01.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const server = http.createServer(function (req, res) {
+    console.log("url",req.url);
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
